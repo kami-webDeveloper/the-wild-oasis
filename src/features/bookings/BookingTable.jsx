9a0c91517daf5ea2c00e9908e6a88b2c35ff55be
kami-webDeveloper <kforoozanfar@gmail.com>
2024-12-26 10:57:47 +0330

    logs removed

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -13,8 +13,6 @@ function BookingTable() {
 
   if (!bookings.data.length) return <Empty resource="Bookings" />;
 
-  console.log(bookings);
-
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
